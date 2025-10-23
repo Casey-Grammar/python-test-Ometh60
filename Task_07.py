b44c55faa76b6796fbc17f648bbeb221074471de
Chris Bucknell <58235679+Buckas@users.noreply.github.com>
2025-10-23 12:46:58 +1100

    Fixed indents in 7 for marking by teacher

--- Task_07.py
+++ Task_07.py
@@ -1,36 +1,36 @@
   #Task 7 Debugging
-  '''
-  Task 7 Debugging (8marks)
+'''
+Task 7 Debugging (8marks)
 
-  The following code uses a function hello to return Hello 
-  and your name. This code currently has some errors and needs 
-  to be debugged to work properly.
+The following code uses a function hello to return Hello 
+and your name. This code currently has some errors and needs 
+to be debugged to work properly.
 
-  This is how it is meant to work
-  =========================
-  What is your name? Ben
-  Your name is too short
-  ========================= 
+This is how it is meant to work
+=========================
+What is your name? Ben
+Your name is too short
+========================= 
 
-  Another example:
-  =========================
-  What is your name? Charlie
-  Hello, Charlie
-  =========================
+Another example:
+=========================
+What is your name? Charlie
+Hello, Charlie
+=========================
 
-  Without removing the function "hello" debug the code so the 
-  tests above produce the given outputs.
+Without removing the function "hello" debug the code so the 
+tests above produce the given outputs.
 
-  '''
+'''
 
-  #===============================
-  # All code to debug is below here
-  #==============================
-  def hello(name):
-      greeting = f'Hello, {name}'
-      return greeting
+#===============================
+# All code to debug is below here
+#==============================
+def hello(name):
+    greeting = (f'Hello, {name}')
+    return greeting
 
-  def main():
+def main():
     name = input('What is your name? ')
     if len(name) < 3:
       print ('Your name is too short')
@@ -38,9 +38,9 @@
       reply = hello(name)
       print(reply)
 
-  #===============================
-  #All code to debug is above here
-  #===============================
+#===============================
+#All code to debug is above here
+#===============================
 
-  if __name__ == '__main__':
+if __name__ == '__main__':
       main()
